d3ab62e9ad52dbb277b00853eca6acd0e7592dea
Beatriz dos Santos Miotto <72702704+BeatrizMiotto@users.noreply.github.com>
2020-10-27 12:28:27 -0300

    conectando site ao banco de dados com PHP
    Página inicial

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Full Stack Eletro</title>
+        <link rel="stylesheet" type="text/css" href="./css/estilo.css">
+    </head>
+    <body>
+        <!--Menu-->
+        <?php
+            include('menu.html');
+        ?>
+
+        <br><br><br><br>
+        <main class="texto_inicial">
+            <h1>Seja bem vindo(a)!</h1>
+            <p>Aqui em nossa loja, programadores têm desconto nos produtos para sua casa!</p>
+        </main>
+        
+
+        <br><br><br><br><br>
+        <br><br><br><br><br>
+        <br><br><br><br><br>
+        <br><br><br><br><br>
+
+        <footer id="rodape">
+            <p id="formas_pagamento">Formas de pagamento:</p>
+            <img class="img_rodape" src="./imagens/WhatsApp Image 2020-09-22 at 21.19.58 (1).jpeg">
+            <p>&copy; Recode Pro</p>
+        </footer>
+        
+    </body>
+</html>
